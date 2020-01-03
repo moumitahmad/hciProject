585bfa0ad2b0d5c7f9ed2df47c69e8e06a2f4494
larsobist <lars.obist@outlook.de>
2020-01-03 19:55:03 +0100

    scenen aufrufen

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+	public void SetText(string text)
+	{
+		Text txt = transform.Find("Text").GetComponent<Text>();
+		txt.text = text;
+	}
+
+    //Von 2 in 1
+    public void SwitchScene1()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+
+    //von 1 in 2
+    public void SwitchScene2()
+    {
+        SceneManager.LoadScene(sceneBuildIndex:0);
+    }
+
+
+}
